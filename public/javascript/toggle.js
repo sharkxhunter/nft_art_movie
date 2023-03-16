5dcd139e78440c158ae105f8c82644c8cd68075f
devking877 <devking877@gmail.com>
2023-03-16 10:22:03 -0400

    nftArt

---
+++ public/javascript/toggle.js
@@ -0,0 +1,8 @@
+function toggle() {
+    var x = document.getElementById('toggle-obj')
+    if (x.style.display === 'none') {
+        x.style.display = 'block'
+    } else {
+        x.style.display = 'none'
+    }
+}
